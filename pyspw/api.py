fdbd81409f875e2913e32d16a042701e237b5712
teleportx <stepan@xstl.ru>
2024-04-18 18:57:40 +0300

    update api interface

--- pyspw/api.py
+++ pyspw/api.py
@@ -1,27 +1,33 @@
-import json
-import platform
-from base64 import b64encode
-from dataclasses import dataclass
-from enum import Enum
-from hashlib import sha256
 import hmac
-import requests as rq
-import time
-from typing import List, Callable
 import logging
-from mojang import API as MAPI
+import time
+from base64 import b64encode
+from functools import cached_property
+from hashlib import sha256
+from typing import List, Callable
+from warnings import warn
+
+from pydantic import BaseModel, computed_field
 
 from . import errors as err
 from . import models
-from .methods.base import RequestTypes
+from .methods import GetUser, CreatePayment, CreateTransaction, GetSelfCardInfo, SetTransactionWebhook, GetMe, \
+    GetUserCards
 
-mapi = MAPI()
 
-
-@dataclass
-class _Card:
+class AuthorizationPair(BaseModel):
     id: str
     token: str
+
+    @computed_field
+    @cached_property
+    def authorization(self) -> str:
+        return f'Bearer ' + str(b64encode(str(f'{self.id}:{self.token}').encode('utf-8')), 'utf-8')
+
+    def verify_webhook(self, webhook_data: str, X_Body_Hash: str):
+        hmac_data = hmac.new(self.token.encode('utf-8'), webhook_data.encode('utf-8'), sha256).digest()
+        base64_data = b64encode(hmac_data)
+        return hmac.compare_digest(base64_data, X_Body_Hash.encode('utf-8'))
 
 
 class SpApi:
@@ -36,9 +42,10 @@ class SpApi:
     """
 
     def __init__(self, card_id: str, card_token: str):
-        self._card = _Card(card_id, card_token)
-        self._authorization = f"Bearer {str(b64encode(str(f'{card_id}:{card_token}').encode('utf-8')), 'utf-8')}"
-
+        self._auth = AuthorizationPair(
+            id=card_id,
+            token=card_token
+        )
 
     def ping(self) -> bool:
         """
@@ -47,7 +54,7 @@ class SpApi:
             :return: Состояние API.
         """
         try:
-            self.get_balance()
+            self.card()
             return True
 
         except err.SpwApiError:
@@ -65,11 +72,16 @@ class SpApi:
             :raises SpwUserNotFound: Пользователь не был найден.
         """
 
-        response = self._request(RequestTypes.GET, f'/users/{discord_id}', ignore_codes=[404])
-        if response.status_code == 404:
-            raise err.SpwUserNotFound(discord_id)
+        try:
+            return GetUser(
+                discord_id=int(discord_id)
+            )(self._auth.authorization)
 
-        return models.User.model_validate_json(response.text)
+        except err.SpwApiError as e:
+            if e.status_code == 404:
+                raise err.SpwUserNotFound
+
+            raise e
 
     def check_access(self, discord_id: str) -> bool:
         """
@@ -80,8 +92,18 @@ class SpApi:
 
             :return: Состояние проходки пользователя.
         """
-        response = self._request(RequestTypes.GET, f'/users/{discord_id}', ignore_codes=[404])
-        return response.status_code != 404
+
+        try:
+            GetUser(
+                discord_id=int(discord_id)
+            )(self._auth.authorization)
+            return True
+
+        except err.SpwApiError as e:
+            if e.status_code == 404:
+                return False
+
+            raise e
 
     def check_webhook(self, webhook_data: str, X_Body_Hash: str) -> bool:
         """
@@ -96,9 +118,7 @@ class SpApi:
             :return: Верефецирован или нет вебхук.
         """
 
-        hmac_data = hmac.new(self._card.token.encode('utf-8'), webhook_data.encode('utf-8'), sha256).digest()
-        base64_data = b64encode(hmac_data)
-        return hmac.compare_digest(base64_data, X_Body_Hash.encode('utf-8'))
+        return self._auth.verify_webhook(webhook_data, X_Body_Hash)
 
     def create_payment(self, payment: models.Payment) -> str:
         """
@@ -109,9 +129,12 @@ class SpApi:
 
             :return: Ссылку на страницу оплаты, на которую стоит перенаправить пользователя.
         """
-        return self._request(RequestTypes.POST, '/payment', payment.model_dump()).json()['url']
 
-    def send_transaction(self, transaction: models.Transaction) -> None:
+        return CreatePayment(
+            payment=payment
+        )(self._auth.authorization).url
+
+    def send_transaction(self, transaction: models.Transaction):
         """
             Отправка транзакции.
             
@@ -121,22 +144,30 @@ class SpApi:
             :raises SpwInsufficientFunds: Недостаточно средств на карте.
             :raises SpwCardNotFound: Карта получателя не найдена.
         """
-        response = self._request(RequestTypes.POST, '/transactions', transaction.model_dump(), ignore_codes=[400])
-        if response.status_code == 400:
-            msg = response.json()["message"]
-            if msg == 'Недостаточно средств на карте':
-                raise err.SpwInsufficientFunds()
 
-            elif msg == 'Карты не существует':
-                raise err.SpwCardNotFound()
+        try:
+            CreateTransaction(
+                transaction=transaction
+            )(self._auth.authorization)
 
-    # def card(self) -> models.SelfCard:
-    #     """
-    #         Получение информации о карте.
-    #
-    #         :return: Card класс с информации о карте.
-    #     """
-    #     return models.SelfCard.model_validate_json(self._request(_RequestTypes.GET, '/card').text)
+        except err.SpwApiError as e:
+            if e.status_code == 400:
+                if e.extra_info == 'Недостаточно средств на карте':
+                    raise err.SpwInsufficientFunds()
+
+                elif e.extra_info == 'Карты не существует':
+                    raise err.SpwCardNotFound()
+
+            raise e
+
+    def card(self) -> models.SelfCard:
+        """
+            Получение информации о токен-карте.
+
+            :return: Card класс с информации о карте.
+        """
+
+        return GetSelfCardInfo()(self._auth.authorization)
 
     def get_balance(self) -> int:
         """
@@ -144,7 +175,44 @@ class SpApi:
 
             :return: Значения баланса карты.
         """
+        warn('Use .card().balance', DeprecationWarning)
+
         return self.card().balance
+
+    def set_transaction_webhook(self, url: str):
+        """
+            Установка адреса для получения вебхуков о транзакциях по карте.
+
+            :param url: Адрес вебхука.
+            :type url: str
+        """
+
+        SetTransactionWebhook(
+            url=url
+        )(self._auth.authorization)
+
+    def me(self) -> models.SelfUser:
+        """
+            Получения пользователя владельца карты.
+
+            :return: Пользователь владельца карты.
+        """
+
+        return GetMe()(self._auth.authorization)
+
+    def get_user_cards(self, username: str) -> List[models.Card]:
+        """
+            Получения карта пользователя.
+
+            :param username: Имя пользователя у которого надо получить карты.
+            :type username: str
+
+            :return: Список карт пользователя.
+        """
+
+        return GetUserCards(
+            username=username
+        )(self._auth.authorization)
 
     # ---------------------------------
     # ------------- Manys -------------
@@ -206,15 +274,18 @@ class SpApi:
         """
         return self._many_req(payments, self.create_payment, delay)
 
-    def send_transactions(self, transactions: List[models.Transaction], delay: float = 0.5) -> None:
+    def send_transactions(self, transactions: List[models.Transaction], delay: float = 0.5, safe: bool = True) -> None:
         """
             Отправка транзакций.
 
             .. warning::
-                **Важно: Перед множетсвенной отправки транзаций проводится дополнительная проверка на количество средств на карте.
+                **Важно: Если safe=true, то перед множетсвенной отправки транзаций проводится дополнительная проверка на количество средств на карте.
                 В случае если во время совершения транзакций кто-либо еще спишет с этой карты сумму, после которой
                 остаток на карте не будет достаточен для проведения транзакции, то выполнение транзакций прервется,
                 а предыдущие транзации не откатятся.**
+
+            :param safe: Значение задержки между запросами, указывается в секундах.
+            :type safe: bool
 
             :param delay: Значение задержки между запросами, указывается в секундах.
             :type delay: float
@@ -227,7 +298,7 @@ class SpApi:
         """
 
         # Additional balance verify
-        if self.get_balance() < sum([tr.amount for tr in transactions]):
+        if safe and self.card().balance < sum([tr.amount for tr in transactions]):
             raise err.SpwInsufficientFunds()
 
         self._many_req(transactions, self.send_transaction, delay)
