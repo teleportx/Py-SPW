b708b4caa957512087701b80bf1ab1479b4b5da0
teleportx <stepan@xstl.ru>
2024-04-19 23:12:00 +0300

    fix main package imports

--- pyspw/__init__.py
+++ pyspw/__init__.py
@@ -1,6 +1,11 @@
 from .api import *
-from . import methods
-from . import errors
-from . import models
+from . import methods, models
 
 __version__ = '2.0.0'
+__all__ = [
+    'SpApi',
+    'models',
+    'methods',
+
+    '__version__',
+]
